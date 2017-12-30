7687670905454a26d0e2cd925e5319b51cebec8a
chazmcgrill <charlietaylordev@gmail.com>
2017-12-30 15:02:11 +0000

    Core Setup
    Created core setup for webpack and react with sass pre-compiler

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import css from './index.sass';
+
+ReactDOM.render(<App />, document.getElementById('root'));
